61d3b20befdc421574a3648229fdcf9c29ae0280
Meisam Taheri <meisam.taheri@inn.no>
2023-10-06 06:28:33 +0200

    splineUpdate

--- Assets/Scripts/SplineCode.cs
+++ Assets/Scripts/SplineCode.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Mathematics;
+using UnityEngine.Splines;
+using UnityEditor;
 
+[ExecuteInEditMode()]
 public class SplineCode : MonoBehaviour
 {
+
+    [SerializeField]
+    SplineContainer newSplineContiner;
+    [SerializeField]
+    int splineIndex;
+    [SerializeField]
+    float speed;
+    Unity.Mathematics.float3 position;
+    float3 tangent;
+    float3 upVector;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +28,13 @@ public class SplineCode : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        newSplineContiner.Evaluate(splineIndex, speed, out position, out tangent, out upVector);
         
+    }
+    public void OnDrawGizmos()
+    {
+        Handles.matrix = transform.localToWorldMatrix;
+        Handles.color = Color.red;
+        Handles.SphereHandleCap(0, position, quaternion.identity, 1f, eventType: EventType.Repaint);
     }
 }
